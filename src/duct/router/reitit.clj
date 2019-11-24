72e2fa8ec37d4eae2a6781ce8a212947613915fd
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-11-24 18:34:33 +0100

    Use reitit's coercion by default

--- src/duct/router/reitit.clj
+++ src/duct/router/reitit.clj
@@ -1,7 +1,19 @@
 (ns duct.router.reitit
   (:require [reitit.core :as r]
             [reitit.ring :as ring]
+            [reitit.ring.coercion :as ring.coercion]
+            [reitit.coercion.spec :as coercion.spec]
+            [duct.core.resource]
             [integrant.core :as ig]))
 
+(def ^:private default-middleware
+  [ring.coercion/coerce-exceptions-middleware
+   ring.coercion/coerce-request-middleware
+   ring.coercion/coerce-response-middleware])
+
 (defmethod ig/init-key :duct.router/reitit [_ {:keys [routes]}]
-  (ring/ring-handler (ring/router routes)))
+  (ring/ring-handler
+   (ring/router
+    routes
+    {:data {:coercion coercion.spec/coercion
+            :middleware default-middleware}})))
--- test/duct/router/reitit_test.clj
+++ test/duct/router/reitit_test.clj
@@ -9,14 +9,31 @@
      :headers {}
      :body (str "Hello " (:name path-params))}))
 
+(defmethod ig/init-key ::handler.math [_ _]
+  (fn [{:keys [parameters]}]
+    (let [{:keys [x y]} (:query parameters)]
+      {:status 200
+       :headers {}
+       :body (str (* x y))})))
+
+(def config
+  {::handler.hello {}
+   ::handler.math {}
+   :duct.router/reitit {:routes ["/"
+                                 ["hello/:name" {:handler (ig/ref ::handler.hello)}]
+                                 ["math" {:get {:parameters {:query {:x int? :y int?}}
+                                                :handler (ig/ref ::handler.math)}}]]}})
+
 (deftest router-test
-  (let [config  {::handler.hello {}
-                 :duct.router/reitit
-                 {:routes   ["/hello/:name" {:get {:name string?}
-                                             :responses {200 {:body string?}}
-                                             :handler (ig/ref ::handler.hello)}]}}
-        handler (:duct.router/reitit (ig/init config))]
-    (is (= {:status 200 :headers {} :body "Hello world"}
-           (handler {:uri "/hello/world"
-                     :request-method :get})))))
+  (let [handler (:duct.router/reitit (ig/init config))]
+    (testing "It hooks up the routes and handlers correctly"
+      (is (= {:status 200 :headers {} :body "Hello world"}
+             (handler {:uri "/hello/world"
+                       :request-method :get}))))
+
+    (testing "It can coerce the input params"
+      (is (= {:status 200 :headers {} :body "12"}
+             (handler {:uri "/math"
+                       :request-method :get
+                       :query-params {:x "3" :y "4"}}))))))
 
